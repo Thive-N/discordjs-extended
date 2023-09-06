f2c50ee05ce18aebc166cc92e64752e98723ba09
Thivejan Neranjan <35178093+Thive-N@users.noreply.github.com>
2023-09-06 15:51:53 +0100

    Moving from Jasmine to Mocha

--- tests/cogManager.test.ts
+++ tests/cogManager.test.ts
@@ -1,35 +1,31 @@
 import { GatewayIntentBits } from 'discord.js';
 import { ExtendedClient } from '../src/types/extendedClient';
+import { assert } from 'chai';
+import { test } from 'mocha';
 
 const c = new ExtendedClient('invalid token', {
     intents: [GatewayIntentBits.MessageContent],
 });
 
-describe('cogToPath', () => {
-    it('should convert a cog string to a path', () => {
-        expect(c.cogManager.cogToPath('test')).toBe('./test.ts');
-        expect(c.cogManager.cogToPath('cogs.test')).toBe('./cogs/test.ts');
-        expect(c.cogManager.cogToPath('examples.cogs.commandcog')).toBe('./examples/cogs/commandcog.ts');
-        expect(c.cogManager.cogToPath('commandcog')).toBe('./commandcog.ts');
-        expect(c.cogManager.cogToPath('..')).toBe('./../.ts');
-        expect(c.cogManager.cogToPath('..test')).toBe('./../test.ts');
-    });
+test('cogToPath', () => {
+    assert.equal(c.cogManager.cogToPath('test'), './test.ts');
+    assert.equal(c.cogManager.cogToPath('cogs.test'), './cogs/test.ts');
+    assert.equal(c.cogManager.cogToPath('examples.cogs.commandcog'), './examples/cogs/commandcog.ts');
+    assert.equal(c.cogManager.cogToPath('commandcog'), './commandcog.ts');
+    assert.equal(c.cogManager.cogToPath('..'), './../.ts');
+    assert.equal(c.cogManager.cogToPath('..test'), './../test.ts');
 });
 
-describe('loadCog', () => {
-    it('should load a command cog', () => {
-        /**
-         * @todo line below has a lot of .'s
-         * this is due to the require stack being in node_modules/jasmine/bin
-         * need to find a way to get the require stack to be in the root of the project
-         */
-        c.cogManager.loadCog('......tests.commandcog');
-        expect(c.commands.has('commandcog')).toBe(true);
-    });
+test('loadCog', () => {
+    /**
+     * @todo line below has a lot of .'s
+     * this is due to the require stack being in node_modules/jasmine/bin
+     * need to find a way to get the require stack to be in the root of the project
+     */
+    c.cogManager.loadCog('............tests.commandcog');
+    assert.equal(c.commands.has('commandcog'), true);
 
-    it('should load a slash command cog', () => {
-        c.cogManager.loadCog('......tests.slashcommandcog');
-        expect(c.slashCommands.has('slashcommandcog')).toBe(true);
-        expect(c.slashCommandsRaw.length).toBe(1);
-    });
+    c.cogManager.loadCog('............tests.slashcommandcog');
+    assert.equal(c.slashCommands.has('slashcommandcog'), true);
+    assert.equal(c.slashCommandsRaw.length, 1);
 });
